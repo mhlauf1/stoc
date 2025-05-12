12675f5cdd6a7f8712900300d78262e1186ed673
mhlauf1 <mlaufersweiler@gmail.com>
2025-05-12 10:52:45 -0500

    fixing hero home spacing

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
       <div className="absolute top-0 left-0 right-0 bottom-0">
         <Image className="-z-1" fill src="/hero-stoc.png" alt="STOC hero" />
       </div>
-      <div className="flex z-10 flex-col w-full px-6 md:px-12 lg:px-32 items-center justify-center">
+      <div className="flex z-10 flex-col gap-6 w-full px-6 md:px-12 lg:px-32 items-center justify-center">
         <NewsLetterBadge />
 
         <h1 className="text-4xl sm:text-5xl text-center tracking-tighter max-w-[20ch] md:text-6xl font-gambetta lg:text-7xl leading-tight text-white">
@@ -18,7 +18,7 @@ export default function Hero() {
             Meet Exceptional Advising
           </span>
         </h1>
-        <p className="mt-6 mb-12 text-md sm:text-lg text-center md:text-xl w-full md:max-w-lg text-white">
+        <p className="text-md sm:text-lg text-center md:text-xl w-full md:max-w-lg text-white">
           Guiding you through every stage of the acquisition lifecycle with
           precision, insight, and proven expertise.{" "}
         </p>
