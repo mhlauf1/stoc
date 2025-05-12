c731dd938c00aaaadf16ae4505ca66980662f39a
mhlauf1 <mlaufersweiler@gmail.com>
2025-05-12 11:12:49 -0500

    fixing about space

--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
   return (
     <section className="bg-[#F7F7F7]  px-4 md:px-12 lg:px-32 py-24 md:py-32">
       <div className="flex flex-col md:flex-row gap-12">
-        <div className="flex flex-col gap-8 md:gap-16 flex-1 items-center md:items-start">
+        <div className="flex flex-col gap-8 flex-1 items-center md:items-start">
           <div className="flex items-center md:mb-4 justify-center gap-4">
             <div className="h-[1px] bg-neutral-300 w-[60px]" />
             <span
