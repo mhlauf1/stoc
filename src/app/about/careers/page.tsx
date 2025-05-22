d2f9fd373b8b453490b8ec001942864cb09e6392
mhlauf1 <mlaufersweiler@gmail.com>
2025-05-22 12:00:06 -0500

    adding careers page

---
+++ src/app/about/careers/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="flex min-h-screen justify-center items-center flex-col px-[5%]">
+      <h1 className="text-[#041E40] text-center text-4xl md:text-5xl  font-gambetta md:max-w-[20ch] leading-tight tracking-tighter">
+        Please send resumes to{" "}
+        <span className="underline font-gambetta">
+          inquiry@stocadvisory.com
+        </span>
+      </h1>
+    </section>
+  );
+};
+
+export default page;
