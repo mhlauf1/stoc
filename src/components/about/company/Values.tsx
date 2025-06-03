7a75d5a7ffb16f160d31a653c3f4f91ca04bb372
mhlauf1 <mlaufersweiler@gmail.com>
2025-06-03 14:34:43 -0500

    adding mirco animations

--- src/components/about/company/Values.tsx
+++ src/components/about/company/Values.tsx
@@ -1,4 +1,7 @@
+// src/components/Values.tsx
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 import { ValueProps } from "@/utils/types";
 import { valuesData } from "@/utils/data";
 
@@ -11,22 +14,27 @@ const ServiceCard: React.FC<ValueProps> = ({ Icon, title, description }) => (
       rounded-2xl border border-[#16333A]
     "
   >
-    <div className="flex flex-row  mb-4 items-center gap-2 md:gap-4">
-      <Icon className="size-5 md:size-6 text-white" />
-
-      <h3 className="text-2xl md:text-3xl lg:text-4xl  max-w-[15ch] tracking-tight font-gambetta">
+    <div className="flex flex-row mb-3 items-center gap-2 md:gap-2">
+      <h3 className="text-2xl md:text-3xl lg:text-4xl max-w-[15ch] tracking-tight font-gambetta">
         {title}
       </h3>
     </div>
-
     <p className="flex-1 text-md md:text-lg tracking-tight">{description}</p>
   </div>
 );
 
 const ServicesGrid: React.FC = () => (
   <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-    {valuesData.map((svc) => (
-      <ServiceCard key={svc.id} {...svc} />
+    {valuesData.map((svc, idx) => (
+      <motion.div
+        key={svc.id}
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut", delay: idx * 0.1 }}
+        viewport={{ once: true, amount: 0.1 }}
+      >
+        <ServiceCard {...svc} />
+      </motion.div>
     ))}
   </div>
 );
