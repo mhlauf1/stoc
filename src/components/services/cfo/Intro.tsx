782e8fe0e04e498f6fee63accd5cafad4ef6caac
mhlauf1 <mlaufersweiler@gmail.com>
2025-06-06 12:08:27 -0500

    updating cfo image

--- src/components/services/cfo/Intro.tsx
+++ src/components/services/cfo/Intro.tsx
@@ -8,7 +8,7 @@ export default function Intro() {
         {/* image column */}
         <div className="relative hidden md:flex w-full h-64 md:h-[50vh]">
           <Image
-            src="/cfo-3.jpeg"
+            src="/cfo-2.jpg"
             alt="CFO Advisory"
             fill
             className="rounded-lg object-cover"
